912588402cffc8b7b312139ac70403513f16f484
mhazoglou <michaelhazoglou@gmail.com>
2026-08-12 00:04:11 -0400

    fixing nvim-treesitter issues

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
-    -- "nvim-treesitter/nvim-treesitter",
-    -- build = ":TSUpdate",
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
 
-    -- config = function()
-    --    local configs = require("nvim-treesitter.configs")
+    config = function()
+       local configs = require("nvim-treesitter.config")
 
-    --     configs.setup({
-    --         auto_install = true,
-    --         -- sync_install = false,
-    --         highlight = { enable = true },
-    --         indent = { enable = true },
-    --     })
-    -- end
+        configs.setup({
+            auto_install = true,
+            -- sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
